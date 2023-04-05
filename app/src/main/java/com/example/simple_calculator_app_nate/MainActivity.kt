79c413297213684402c6dead9c4f867078f6b5a3
SalomeNderu <sallienderu@gmail.com>
2023-04-05 10:38:23 +0300

    simple calculator

--- app/src/main/java/com/example/simple_calculator_app_nate/MainActivity.kt
+++ app/src/main/java/com/example/simple_calculator_app_nate/MainActivity.kt
@@ -12,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var edtFirst:EditText
     private lateinit var edtSecond:EditText
     private lateinit var add_button:Button
+    private lateinit var multiply_button:Button
+    private lateinit var divide_button:Button
+    private lateinit var subtract_button:Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         edtFirst=findViewById(R.id.edtFirstNumber)
         edtSecond=findViewById(R.id.edtSecondNumber)
         add_button=findViewById(R.id.btnAdd)
+        multiply_button=findViewById(R.id.btnMultiply)
+        divide_button=findViewById(R.id.btnDivide)
+        subtract_button=findViewById(R.id.btnSubtract)
 
         add_button.setOnClickListener {
             var first_num = edtFirst.text.toString().trim()
@@ -37,6 +43,48 @@ class MainActivity : AppCompatActivity() {
 //                Toast.makeText(this, "Trying to add two numbers", Toast.LENGTH_SHORT).show()
             }
 //            Toast.makeText(this, "HELLO World", Toast.LENGTH_SHORT).show()
+        }
+
+        multiply_button.setOnClickListener {
+            var first_num = edtFirst.text.toString().trim()
+            var second_num = edtSecond.text.toString().trim()
+            //user validation
+            if (first_num.isEmpty() || second_num.isEmpty()){
+                Toast.makeText(this, "PLEASE ENETR A NUMBER", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                var answer = first_num.toDouble() * second_num.toDouble()
+                txt_display_answer.text = answer.toString()
+//                Toast.makeText(this, "Trying to add two numbers", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        divide_button.setOnClickListener {
+            var first_num = edtFirst.text.toString().trim()
+            var second_num = edtSecond.text.toString().trim()
+            //user validation
+            if (first_num.isEmpty() || second_num.isEmpty()){
+                Toast.makeText(this, "PLEASE ENETR A NUMBER", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                var answer = first_num.toDouble() / second_num.toDouble()
+                txt_display_answer.text = answer.toString()
+//                Toast.makeText(this, "Trying to add two numbers", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        subtract_button.setOnClickListener {
+            var first_num = edtFirst.text.toString().trim()
+            var second_num = edtSecond.text.toString().trim()
+            //user validation
+            if (first_num.isEmpty() || second_num.isEmpty()){
+                Toast.makeText(this, "PLEASE ENETR A NUMBER", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                var answer = first_num.toDouble() - second_num.toDouble()
+                txt_display_answer.text = answer.toString()
+//                Toast.makeText(this, "Trying to add two numbers", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
